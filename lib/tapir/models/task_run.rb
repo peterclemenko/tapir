e1e42bf2e22d88c98cb6442e6de1beaf9422dfde
jcran <jcran@pentestify.com>
2013-06-29 13:03:07 -0500

    show more direct task run relationships in the UI

--- lib/tapir/models/task_run.rb
+++ lib/tapir/models/task_run.rb
@@ -12,17 +12,19 @@ class TaskRun
   field :task_options_hash, type: String
   field :task_log, type: String
 
+  has_many :entity_mappings
+
   def to_s
     "#{task_name} task -> #{task_entity_type}:#{task_entity_id}"
   end
 
-  def entity_mappings
-    EntityMapping.any_of(
-      {"$and" => [{:child_id => self.task_entity_id},
-        {:child_type => self.task_entity_type}]}, 
-      {"$and" => [{:parent_id => self.task_entity_id},
-        {:parent_type => self.task_entity_type}]})
-  end
+  #def entity_mappings
+  #  EntityMapping.any_of(
+  #    {"$and" => [{:child_id => self.task_entity_id},
+  #      {:child_type => self.task_entity_type}]}, 
+  #    {"$and" => [{:parent_id => self.task_entity_id},
+  #      {:parent_type => self.task_entity_type}]})
+  #end
 
 end
 end
