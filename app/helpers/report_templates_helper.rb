56faa9e5fc0b4f9541fdd8af584019e7333950ca
jcran <jcran@pentestify.com>
2013-07-16 21:39:00 -0500

    updated recursion for children reports

--- app/helpers/report_templates_helper.rb
+++ app/helpers/report_templates_helper.rb
@@ -1,2 +1,12 @@
 module ReportTemplatesHelper
+
+def render_children(item,result)
+  result << "<ul>" 
+    result << "<li>" << link_to(item, tapir_entity_path(item.id.to_s)) << "</li>" 
+    item.children.each {|x| render_children(x,result) }
+  result << "</ul>"
+result
+end
+
+
 end
--- lib/tapir/models/report_template.rb
+++ lib/tapir/models/report_template.rb
@@ -2,6 +2,9 @@ module Tapir
   class ReportTemplate
     include Mongoid::Document
     field :name, type: String
+    field :pretty_name, type: String
+    field :template, type: String
+    field :setup, type: String
 
     validates_uniqueness_of :name  
   end
