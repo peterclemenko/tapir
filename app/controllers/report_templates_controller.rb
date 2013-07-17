4386a0f63a04c9a0a2bbaee249208809e072c45c
jcran <jcran@pentestify.com>
2013-07-16 20:41:04 -0500

    Make reports a bit more dynamic

--- app/controllers/report_templates_controller.rb
+++ app/controllers/report_templates_controller.rb
@@ -14,14 +14,8 @@ class ReportTemplatesController < ApplicationController
   # GET /report_templates/1.json
   def show
     @report_template = Tapir::ReportTemplate.find(params[:id])
-
-    if @report_template.name == "Findings Report"
-      @entities = Tapir::Entities::Finding.all
-    elsif @report_template.name == "Organization Report"
-      @entities = Tapir::Entities::Organization.all
-    else
-      @entities = Tapir::Entities::Base.all
-    end
+    
+    eval(@report_template.setup)
 
     respond_to do |format|
       format.html # show.html.erb
--- lib/tapir/models/report_template.rb
+++ lib/tapir/models/report_template.rb
@@ -1,9 +1,7 @@
 module Tapir
   class ReportTemplate
     include Mongoid::Document
-
     field :name, type: String
-    field :content, type: String
 
     validates_uniqueness_of :name  
   end
