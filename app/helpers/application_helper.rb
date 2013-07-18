8ad73824cc4666022c2cae2d1abbd0019f30aef3
jcran <jcran@pentestify.com>
2013-07-18 16:25:48 -0500

    slight report cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,27 +22,17 @@ module ApplicationHelper
     return "" if item.parents.empty?
         
     result << "<ul>"
-
-    
-    # Call render on each of the parents
-    item.parents.each do |x|
-          
-    end
-
-
     # Print all the parents
     item.parents.each do |x|
       result << print_result(x)
       render_parents(x,result)
     end
 
-    
     result << "</ul>"
   result
   end
 
   def print_result(item)
-    #binding.pry
     # << " (#{item.task_runs.where(:task_entity_id => item.id).first}) " 
     "<li>" << link_to(item, tapir_entity_path(item.id.to_s)) << "</li>"   
   end
