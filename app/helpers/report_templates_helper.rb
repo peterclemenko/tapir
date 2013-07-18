d2d8961f4fd60d1340ec0b0dbb4cd66b9fbb378e
jcran <jcran@pentestify.com>
2013-07-17 19:49:48 -0500

    remove old database schema, display (all) children / parents when showing an entity

--- app/helpers/report_templates_helper.rb
+++ app/helpers/report_templates_helper.rb
@@ -1,12 +1,3 @@
 module ReportTemplatesHelper
 
-def render_children(item,result)
-  result << "<ul>" 
-    result << "<li>" << link_to(item, tapir_entity_path(item.id.to_s)) << "</li>" 
-    item.children.each {|x| render_children(x,result) }
-  result << "</ul>"
-result
-end
-
-
 end
