7830b44679d9debff1652a1f12b1764aafbe9cd8
jcran <jcran@pentestify.com>
2013-07-21 15:58:36 -0500

    clean up social profile search

--- lib/tapir/tasks/social_profile_search.rb
+++ lib/tapir/tasks/social_profile_search.rb
@@ -13,8 +13,7 @@ end
 
 ## Returns an array of types that are allowed to call this task
 def allowed_types
-  [ Tapir::Entities::SearchString,
-    Tapir::Entities::Person,
+  [ Tapir::Entities::Person,
     Tapir::Entities::Username]
 end
 
@@ -35,17 +34,17 @@ def run
   # Store the account name, depending on the entity we passed in.
   #
   if @entity.kind_of? Tapir::Entities::Person
-    account_names = @entity.usernames.all
+    usernames = @entity.usernames.all
   else
-    account_names = [@entity.name]
+    usernames = [@entity.name]
   end
   
   #
   # Iterate through all account names!
   #
-  account_names.each do |account_name|
+  usernames.each do |username|
 
-    @task_logger.log "Looking for account name: #{account_name}"
+    @task_logger.log "Looking for account name: #{username.name}"
     
     ###
     ### Loop through services, creating a client & checking
@@ -57,28 +56,20 @@ def run
       # Create a client of the current type
       # 
       client = eval("Tapir::Client::#{client_name}::WebClient.new")
-      if client.check_account_exists(account_name)
-        @task_logger.log "Found an account at: #{client.check_account_uri_for(account_name)}"
+      if client.check_account_exists(username.name)
+        @task_logger.log "Found an account at: #{client.check_account_uri_for(username.name)}"
         
         #
         # Create an account w/ the correct parameters
         #
         obj = create_entity Tapir::Entities::Account, 
-        :account_name => account_name, 
+        :account_name => username.name, 
         :service_name => client.service_name, 
-        :check_uri => client.check_account_uri_for(account_name),
-        :web_uri => client.web_account_uri_for(account_name)
-
-        #
-        # If we passed in a user, associate that with our new account entity
-        #
-        #if @entity.kind_of? User
-        #  obj.user_id = @entity.id
-        #  obj.save!
-        #end
+        :check_uri => client.check_account_uri_for(username.name),
+        :web_uri => client.web_account_uri_for(username.name)
         
       else 
-        @task_logger.log "No #{client.service_name} account found at: #{client.check_account_uri_for(account_name)}"
+        @task_logger.log "No #{client.service_name} account found at: #{client.check_account_uri_for(username.name)}"
       end
     end
     
