51b58f45a020c53a9989985c3a2f4c133efc1b71
jcran <jcran@pentestify.com>
2013-07-25 15:33:26 -0500

    rework netblocks created via whois

--- lib/tapir/models/entities/net_block.rb
+++ lib/tapir/models/entities/net_block.rb
@@ -3,10 +3,19 @@ module Tapir
     class NetBlock < Base
       include TenantAndProjectScoped
 
-      field :range, type: String 
-      field :handle, type: String
+      field :start_address, type: String 
+      field :end_address, type: String 
+      field :cidr, type: String
+      field :block_type, type: String
       field :description, type: String
-      
+      field :handle, type: String
+      field :org_ref, type: String
+      field :parent_ref, type: String
+
+      def range
+        "#{start_address}/#{cidr}"
+      end
+
     end
   end
 end
