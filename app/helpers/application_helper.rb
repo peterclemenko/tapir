a718e0f173b39fc98d5ff874fdbf794f078231f9
jcran <jcran@pentestify.com>
2013-08-16 17:44:25 -0500

    render parents in the correct order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,48 @@
 module ApplicationHelper
 
   def render_children(entity, result = "")
+    # Base case 
     return result = "" if entity.children.empty?
     
     result << "<ul>"  
     entity.children.each do |x|
       result << print_result(x)
       next if x.children.include? entity
-
       render_children(x, result) 
     end
     result << "</ul>"
 
-  result
+  result 
   end
 
-  def render_parents(entity, result = "")
-    return result = "" if entity.parents.empty?
+
+  def render_parents(entity, result = "", depth=0)
     
-    result << "<ul>"
-    entity.parents.each do |x|
-
-      result << print_result(x)
-
-      next if x.parents.include? entity
-
-      render_parents(x, result)
+    if entity.parents.empty?
+      # Base case - Close up the upper-lists
+      depth.times do result << "</ul>" end
+      return result
+    else
+      depth = depth + 1
     end
-    result << "</ul>"
+    
+    entity.parents.each do |x|
+      next if x.parents.include? entity
+      
+      # Print this parent at the beginning of the string
+      result.prepend print_result(x)
+      result.prepend "<ul>"
 
-  result
+      # Recurse on the child
+      render_parents(x, result, depth)
+
+    end
+    
+
+    result
   end
+
+
 
   def print_result(entity)
     # << " (#{item.task_runs.where(:task_entity_id => item.id).first}) " 
