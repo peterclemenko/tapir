c6e18bc4a0a392f2c628f719147d49eca6fa6f7b
jcran <jcran@pentestify.com>
2013-08-20 18:09:02 -0500

    add a ghetto way to grab the tld

---
+++ lib/tapir/tasks/dns_tld.rb
@@ -0,0 +1,37 @@
+require 'resolv'
+
+def name
+  "dns_tld"
+end
+
+def pretty_name
+  "DNS TLD"
+end
+
+# Returns a string which describes what this task does
+def description
+  "Add an entity for record's TLD"
+end
+
+# Returns an array of valid types for this task
+def allowed_types
+  [Entities::DnsRecord]
+end
+
+## Returns an array of valid options and their description/type for this task
+def allowed_options
+ []
+end
+
+def setup(entity, options={})
+  super(entity, options)  
+  self
+end
+
+# Default method, subclasses must override this
+def run
+  super
+
+  @entity.name.split('.').pop(2).join(".")
+
+end
