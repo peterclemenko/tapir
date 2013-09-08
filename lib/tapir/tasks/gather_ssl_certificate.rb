457f72e1298722b6de36d4cffee56d21e2b2cdf6
jcran <jcran@pentestify.com>
2013-09-07 22:45:13 -0500

    catch an error on the ssl certificate task, set render parents depth correctly

--- lib/tapir/tasks/gather_ssl_certificate.rb
+++ lib/tapir/tasks/gather_ssl_certificate.rb
@@ -32,22 +32,26 @@ end
 def run
   super
 
-  # Create a socket and connect
-  tcp_client = TCPSocket.new @entity.host.name, @entity.port_num
-  ssl_client = OpenSSL::SSL::SSLSocket.new tcp_client
-  
-  # Grab the cert
-  ssl_client.connect
+  begin
+    # Create a socket and connect
+    tcp_client = TCPSocket.new @entity.host.name, @entity.port_num
+    ssl_client = OpenSSL::SSL::SSLSocket.new tcp_client
+    
+    # Grab the cert
+    ssl_client.connect
 
-  # Parse the cert
-  cert = OpenSSL::X509::Certificate.new(ssl_client.peer_cert)
-  
-  # Close the sockets
-  ssl_client.sysclose
-  tcp_client.close
+    # Parse the cert
+    cert = OpenSSL::X509::Certificate.new(ssl_client.peer_cert)
+    
+    # Close the sockets
+    ssl_client.sysclose
+    tcp_client.close
 
-  # Create an SSL Certificate entity
-  create_entity Entities::SslCert, { :name => cert.subject, :text => cert.to_text } 
+    # Create an SSL Certificate entity
+    create_entity Entities::SslCert, { :name => cert.subject, :text => cert.to_text } 
+  rescue OpenSSL::SSL::SSLError => e
+    @task_logger.log "Caught an error: #{e}"
+  end
 end
 
 def cleanup
