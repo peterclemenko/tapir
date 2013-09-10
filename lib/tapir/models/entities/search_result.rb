c0fbe62c351832803ee20920c0169d9abb82517e
jcran <jcran@pentestify.com>
2013-09-10 12:10:02 -0500

    link search results

--- lib/tapir/models/entities/search_result.rb
+++ lib/tapir/models/entities/search_result.rb
@@ -3,7 +3,6 @@ module Entities
   class SearchResult < Base
     include TenantAndProjectScoped
 
-    field :link, type: String
     field :url, type: String
     field :content, type: String 
      
