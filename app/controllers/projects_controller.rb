cf6173a6b60c905f90d3e456763b6f516c62acb1
jcran <jcran@pentestify.com>
2013-10-23 23:21:50 -0500

    project update  will work if the right param is used. fixes #74

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -76,7 +76,7 @@ class ProjectsController < ApplicationController
   def update
     @project = Project.find(params[:id])
     respond_to do |format|
-      if @project.update_attributes(params[:tapir_project])
+      if @project.update_attributes(params[:project])
         format.html { redirect_to project_path(@project), notice: 'Project was successfully updated.' }
         format.json { head :ok }
       else
