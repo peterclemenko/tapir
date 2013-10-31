f16d08fd4a294b1de05397ac6f43fadac306a303
jcran <jcran@pentestify.com>
2013-10-30 20:48:58 -0500

    add twitter account tasks back in

--- lib/tapir/tasks/twitpic_photo_locations.rb
+++ lib/tapir/tasks/twitpic_photo_locations.rb
@@ -17,7 +17,7 @@ end
 
 # Returns an array of types that are allowed to call this task
 def allowed_types
-  [Entities::Person]
+  [Entities::Username, Entities::TwitterAccount]
 end
 
 ## Returns an array of valid options and their description/type for this task
@@ -34,45 +34,46 @@ def run
   super
 
   x = Client::TwitPic::TwitPicScraper.new
-  @entity.usernames.each do |username|
-    photos = x.search_by_user "#{username}"
-      
-    photos.each do |photo|
+  username = @entity.name
 
-      begin
-        # Analyze the photo
-        pic_data = EXIFR::JPEG.new(photo.local_path) #/tmp/twitpic_file_35193:
+  photos = x.search_by_user "#{username}"
+    
+  photos.each do |photo|
 
-        if pic_data.gps_latitude && pic_data.gps_longitude
-          @task_logger.good "Parsing exif data!"
-          lat = pic_data.gps_latitude[0].to_f + (pic_data.gps_latitude[1].to_f / 60) + (pic_data.gps_latitude[2].to_f / 3600)
-          long = pic_data.gps_longitude[0].to_f + (pic_data.gps_longitude[1].to_f / 60) + (pic_data.gps_longitude[2].to_f / 3600)
-          if pic_data.gps_longitude_ref && pic_data.gps_latitude_ref
-            long = long * -1 if pic_data.gps_longitude_ref == "W"   # (W is -, E is +)
-            lat = lat * -1 if pic_data.gps_latitude_ref == "S"      # (N is +, S is -)
-          else
-            @task_logger.log "no gps_latitude_ref / gps_longitude_ref. not adjusting"
-          end
-          
-          #
-          # Don't create them if they're useless
-          #
-          unless lat == 0 and long == 0
-            create_entity(PhysicalLocation, {:latitude => "#{lat}",  :longitude => "#{long}"})
-          end
-          
+    begin
+      # Analyze the photo
+      pic_data = EXIFR::JPEG.new(photo.local_path) #/tmp/twitpic_file_35193:
+
+      if pic_data.gps_latitude && pic_data.gps_longitude
+        @task_logger.good "Parsing exif data!"
+        lat = pic_data.gps_latitude[0].to_f + (pic_data.gps_latitude[1].to_f / 60) + (pic_data.gps_latitude[2].to_f / 3600)
+        long = pic_data.gps_longitude[0].to_f + (pic_data.gps_longitude[1].to_f / 60) + (pic_data.gps_longitude[2].to_f / 3600)
+        if pic_data.gps_longitude_ref && pic_data.gps_latitude_ref
+          long = long * -1 if pic_data.gps_longitude_ref == "W"   # (W is -, E is +)
+          lat = lat * -1 if pic_data.gps_latitude_ref == "S"      # (N is +, S is -)
         else
-          @task_logger.log "no gps_latitude / gps_longitude. no exif data to parse"
+          @task_logger.log "no gps_latitude_ref / gps_longitude_ref. not adjusting"
         end
-      rescue EXIFR::MalformedJPEG => e
-        @task_logger.error "Unable to parse, malformed jpg"
+        
+        #
+        # Don't create them if they're useless
+        #
+        unless lat == 0 and long == 0
+          create_entity(PhysicalLocation, {:latitude => "#{lat}",  :longitude => "#{long}"})
+        end
+        
+      else
+        @task_logger.log "no gps_latitude / gps_longitude. no exif data to parse"
       end
-
-      create_entity Entities::RemoteImage, 
-        :name => photo.remote_path, 
-        :description => "twitpic image"
+    rescue EXIFR::MalformedJPEG => e
+      @task_logger.error "Unable to parse, malformed jpg"
     end
+
+    create_entity Entities::RemoteImage, 
+      :name => photo.remote_path, 
+      :description => "twitpic image"
   end
+
 end
 
 def cleanup
